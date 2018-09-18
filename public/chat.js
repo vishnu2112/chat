f20de6a77c86ece6806c28592a54b9fcffed17e8
vishn_u <vp493137@gmail.com>
2018-09-19 00:27:53 +0530

    added javascript code to check the msg

--- public/chat.js
+++ public/chat.js
@@ -8,17 +8,63 @@ btn.addEventListener('click', function(){
     socket.emit('chat', {
         message: message.value,
         username: username.value
-    });
-    message.value = "";
+    });    
 });
-message.addEventListener('keypress', function(){
-    socket.emit('typing', username.value);
-})
+
+
 socket.on('chat', function(data){
     feedback.innerHTML = '';
     output.innerHTML += '<p><strong>' + data.username + ': </strong>' + data.message + '</p>';
+    
 });
 
-socket.on('typing', function(data){
-    feedback.innerHTML = '<p><em>' + data + ' is typing a message...</em></p>';
+socket.on('chat',function(data){
+	if(data.message == "hii"){
+	
+		feedback.innerHTML = '';
+	    output.innerHTML += '<p> <strong>Frnd: </strong>'+ "hii " + data.username + '</p>';
+	    feedback.innerHTML = '';
+	    output.innerHTML += '<p> <strong>Frnd: </strong>'+ "How r u ??" + '</p>';
+	}
+	else if(data.message == "i am fine"){
+		feedback.innerHTML = '';
+	    output.innerHTML += '<p> <strong>Frnd: </strong>'+ "Good " + '</p>';
+	    feedback.innerHTML = '';
+	    output.innerHTML += '<p> <strong>Frnd: </strong>'+ "Which class are you studying?" + '</p>';
+	    
+	}
+	else if(data.message == "how r u ?"){
+		feedback.innerHTML = '';
+	    output.innerHTML += '<p> <strong>Frnd: </strong>'+ "i am fine" + '</p>';
+	    feedback.innerHTML = '';
+	    output.innerHTML += '<p> <strong>Frnd: </strong>'+ "how r ur parents and sis?" + '</p>';
+	}
+	else if(data.message == "B-tech"){
+		feedback.innerHTML = '';
+	    output.innerHTML += '<p> <strong>Frnd: </strong>'+ "Good " + '</p>';
+	    feedback.innerHTML = '';
+	    output.innerHTML += '<p> <strong>Frnd: </strong>'+ "How is your studies going on?" + '</p>';
+	}
+	else if(data.message == "B-tech"){
+		feedback.innerHTML = '';
+	    output.innerHTML += '<p> <strong>Frnd: </strong>'+ "Good " + '</p>';
+	    feedback.innerHTML = '';
+	    output.innerHTML += '<p> <strong>Frnd: </strong>'+ "How can I help u?" + '</p>';
+
+	}
+	else if(data.message == "I am interested to join foss"){
+		feedback.innerHTML = '';
+	    output.innerHTML += '<p> <strong>Frnd: </strong>'+ "ohh fine" + '</p>';
+	    feedback.innerHTML = '';
+	    output.innerHTML += '<p> <strong>Frnd: </strong>'+ "R u from CSE background?" + '</p>';
+	    
+	}
+	
+	
+	else{
+		feedback.innerHTML = '';
+	    
+	}
+	
 });
+
